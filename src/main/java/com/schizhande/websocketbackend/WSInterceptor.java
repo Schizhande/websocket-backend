4a5bf8b29c13804699f8eaf71d197520a36f6691
sheldon <simba@codevirtus.com>
2022-04-05 12:07:40 +0200

    able to get token

--- src/main/java/com/schizhande/websocketbackend/WSInterceptor.java
+++ src/main/java/com/schizhande/websocketbackend/WSInterceptor.java
@@ -2,15 +2,16 @@ package com.schizhande.websocketbackend;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,29 +20,43 @@ public class WSInterceptor implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
 
-        log.debug("### Web Socket Interceptor");
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
 
-        val headers = message.getHeaders();
-        val accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+            if (raw instanceof Map) {
+                Object name = ((Map) raw).get("token");
+                log.info("Token {}", name);
 
-        log.info("### headers {}", headers);
-        String requestTokenHeader = "";
-
-        val messageType = (SimpMessageType) headers.get(StompHeaderAccessor.MESSAGE_TYPE_HEADER);
-
-        log.debug("### message type {}", messageType);
-
-        if (messageType == SimpMessageType.CONNECT) {
-
-            accessor.setUser(new WSPrincipal("admin"));
-            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                    null, null, null);
-
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-
-
+                if (name instanceof ArrayList) {
+                    accessor.setUser(new WSPrincipal(((ArrayList<String>) name).get(0).toString()));
+                }
+            }
         }
+//        log.debug("### Web Socket Interceptor");
+//
+//
+//        val headers = message.getHeaders();
+//        val accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+//
+//        log.info("### headers {}", headers);
+//        String requestTokenHeader = "";
+//
+//        val messageType = (SimpMessageType) headers.get(StompHeaderAccessor.MESSAGE_TYPE_HEADER);
+//
+//        log.debug("### message type {}", messageType);
+//
+//        if (messageType == SimpMessageType.CONNECT) {
+//
+//            accessor.setUser(new WSPrincipal("admin"));
+//            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+//                    null, null, null);
+//
+//            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+//
+//
+//        }
 
         return message;
     }
