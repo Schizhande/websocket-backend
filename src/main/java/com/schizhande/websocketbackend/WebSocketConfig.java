7be946abd58b1c0fd5f0d550192f680af24f311a
sheldon <simba@codevirtus.com>
2022-04-05 14:28:13 +0200

    working example done

--- src/main/java/com/schizhande/websocketbackend/WebSocketConfig.java
+++ src/main/java/com/schizhande/websocketbackend/WebSocketConfig.java
@@ -22,7 +22,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/greeting")
-                .setHandshakeHandler(new CustomHandshakeHandler())
                 .setAllowedOriginPatterns("*");
     }
 
